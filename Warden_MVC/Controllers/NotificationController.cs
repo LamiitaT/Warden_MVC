cecc1f0690fc0af2aff30ff5982c48cf002d6d5e
LamiitaT <220394@via.dk>
2017-12-10 20:16:02 +0100

    Notifications almost done

--- Warden_MVC/Controllers/NotificationController.cs
+++ Warden_MVC/Controllers/NotificationController.cs
@@ -11,43 +11,57 @@ namespace Warden_MVC.Controllers
     public class NotificationController : Controller
     {
         // GET: Notification
-        public ViewResult Index(int? page)
+        public ViewResult Index()
         {
-            
+            return View();
+          
+        }
+        public ActionResult ListNotif(int? page)
+        {
             List<notifications> notification_list = new List<notifications>();
             using (wardenEntities wardenEntity = new wardenEntities())
             {
                 notification_list = wardenEntity.notifications.ToList<notifications>();
             }
-            int pageSize = 3;
+
+            int pageSize = 5;
             int pageNumber = (page ?? 1);
-            return View(notification_list.ToPagedList(pageNumber,pageSize));
-          
+            return View(notification_list.ToPagedList(pageNumber, pageSize));
         }
-        public ActionResult Gethistory()
+        public ViewResult Gethistory()
+        {
+            return View();
+        }
+        public ActionResult ListHistory(int? page)
         {
             List<history> history_list = new List<history>();
             using (wardenEntities wardenEntity = new wardenEntities())
             {
                 history_list = wardenEntity.history.ToList<history>();
             }
-            return View(history_list);
 
-
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            return View(history_list.ToPagedList(pageNumber, pageSize));
         }
-        public ActionResult getall()
+        public void Seen(int id)
         {
-            return PartialView();
-        }
-        public IList<notifications> getnotifications()
-        {
-           
-            List<notifications> history_list = new List<notifications>();
+            notifications notification = new notifications();
+            history h = new history();
             using (wardenEntities wardenEntity = new wardenEntities())
             {
-                history_list = wardenEntity.notifications.ToList<notifications>();
+                notification = wardenEntity.notifications.Where(x => x.idn == id).FirstOrDefault();
+                wardenEntity.notifications.Remove(notification);
+                h.idn = id;
+                h.idb = notification.idb;
+                h.ninfo = notification.ninfo;
+                h.ntype = notification.ntype;
+                h.status = this.HttpContext.User.Identity.Name;
+                h.time = notification.time;
+                wardenEntity.history.Add(h);
+                wardenEntity.SaveChanges();
             }
-            return history_list;
+            
         }
         public ActionResult SelectStatus()
         {
